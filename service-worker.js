b448b3c683c97ca8aef51a563c62a944209995d2
Ryan Adams <rradams16@gmail.com>
2026-08-24 17:02:45 -0400

    Fix PWA cache updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flip7-scorer-v2";
+const CACHE_NAME = "flip7-scorer-v3";
 
 const FILES_TO_CACHE = [
     "./",
@@ -9,40 +9,78 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache => {
+
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
+
+            })
+
     );
 
+    // Activate the new worker immediately.
     self.skipWaiting();
+
 });
 
 
 self.addEventListener("activate", event => {
+
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
+
+        caches.keys()
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys
+                        .filter(
+                            key =>
+                                key !== CACHE_NAME
+                        )
+                        .map(
+                            key =>
+                                caches.delete(key)
+                        )
+
+                );
+
+            })
+            .then(() => {
+
+                // Take control of already-open
+                // Flip 7 pages immediately.
+                return self.clients.claim();
+
+            })
+
     );
 
-    self.clients.claim();
 });
 
 
 self.addEventListener("fetch", event => {
+
     event.respondWith(
+
         caches.match(event.request)
             .then(cachedResponse => {
 
                 if (cachedResponse) {
+
                     return cachedResponse;
+
                 }
 
                 return fetch(event.request);
+
             })
+
     );
+
 });
